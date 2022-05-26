4f2bfc3a4c781eb507632c5e446c26a43ef44384
tayfundagci <tayfundagci3517@gmail.com>
2022-05-26 09:45:31 +0300

    styling profile page

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,7 +1,22 @@
 import React from "react";
 
 function Profile() {
-  return <div className="text-2xl text-gray-400 px-10 py-10">Profile</div>;
+  return (
+    <div className="text-2xl text-gray-400 px-10 py-10">
+      <div className=" grid md:grid-cols-3  gap-4">
+        <div className="bg-orange-600 col-span-1">
+          <h1 className="text-2xl text-gray-400 px-10 py-10">Profile</h1>
+        </div>
+        <div className="bg-orange-200 col-span-2">
+          {" "}
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci,
+          quidem perspiciatis voluptatibus facilis veritatis at laudantium
+          commodi animi sapiente id quisquam voluptas illo dignissimos enim quo
+          cum consequatur expedita mollitia.
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Profile;
